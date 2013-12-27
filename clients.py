787494af73a0b0e316547c3ec8536aa9ac21575e
= <ccandillo@rocketmail.com>
2013-12-27 14:18:08 -0600

    Allow the user to specify a hostname and port

--- clients.py
+++ clients.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 
+import argparse
 from socket import *
 
 HOST = 'localhost'
 PORT = 21567
 BUFSIZ = 1024
-ADDR = (HOST, PORT)
+
+parser = argparse.ArgumentParser(description='Allow the user to specify a hostname and a port.')
+parser.add_argument('--hostname', default=HOST, help='Add hostname')
+parser.add_argument('--port', default=PORT, help='Add port')
+args = parser.parse_args()
+
+ADDR = (args.hostname, int(args.port))
 
 while True:
     tcpCliSock = socket(AF_INET, SOCK_STREAM)

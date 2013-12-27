2f5751fa48d3ef5f3e4c779140375536449d81f0
= <ccandillo@rocketmail.com>
2013-12-27 16:59:26 -0600

    adding date command to echo server (not working yet)

---
+++ echo_client.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import socket
+
+HOST = 'localhost'
+PORT = 50007
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+#s.sendall('Hello, world')
+data = s.recv(1024)
+#s.close()
+print 'Received', repr(data)
+
